961ee5189cb238a98525d05f2c0c540e6cdbb21a
Latifaia <latifaia331@gmail.com>
2023-02-05 10:35:56 +0300

    neww

--- Health-Wave/general.js
+++ Health-Wave/general.js
@@ -1,7 +1,18 @@
-const changeTheme = (theme) => {
+/*const changeTheme = (theme) => {
     document.body.classList = '';
     document.body.classList.add(theme);
-}
+}*/
+
+const setTheme = theme => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  };
+  
+  const savedTheme = localStorage.getItem("theme");
+  
+  if (savedTheme) {
+    document.documentElement.className = savedTheme;
+  }
 
 
 
